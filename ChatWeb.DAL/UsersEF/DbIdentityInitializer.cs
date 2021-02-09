6d2c13d669e2df272c33c65bbc58e49dcfbc1a21
Constantine <constantine.vershinin.1992@gmail.com>
2021-02-10 00:15:37 +0600

    DbRoleInitializer

---
+++ ChatWeb.DAL/UsersEF/DbIdentityInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWeb.DAL.UsersEF
+{
+    class DbIdentityInitializer: DropCreateDatabaseIfModelChanges<ApplicationContext>
+    {
+        protected override void Seed(ApplicationContext context)
+        {
+            
+        }
+    }
+}
